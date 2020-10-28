ee0b971601eca4c77fab080087e28bf9fa5b068b
Stark <stark.cheng@outlook.com>
2020-10-27 20:57:10 -0400

    Update inception.py

--- CNNs/inception.py
+++ CNNs/inception.py
@@ -3,32 +3,39 @@ from keras.applications.inception_v3 import InceptionV3, preprocess_input
 from keras.models import Model, Sequential
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.preprocessing.image import ImageDataGenerator
-from keras.optimizers import Adam
-from keras.layers import Dense, Dropout, GlobalAveragePooling2D
+from keras.optimizers import Adam, SGD, RMSprop
+from keras.layers import Dense, Dropout, GlobalAveragePooling2D, Flatten
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
 
-base_model = InceptionV3(weights='imagenet', include_top=False)
-x = base_model.output
-x = GlobalAveragePooling2D(name='avg_pool')(x)
+base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=(299, 299, 3))
+x = Flatten()(base_model.output)
+x = Dense(4096, activation='relu')(x)
+x = Dense(2048, activation='relu')(x)
 x = Dropout(0.4)(x)
-predictions = Dense(2, activation='softmax')(x)
+x = Dense(1024, activation='relu')(x)
+predictions = Dense(1, activation='softmax')(x)
 model = Model(inputs=base_model.input, outputs=predictions)
 
 for layer in base_model.layers:
     layer.trainable = False
 
-model.compile(optimizer='adam',
+model.compile(optimizer=Adam(lr=0.001),
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
+print(model.summary())
+
 train_datagen = ImageDataGenerator(
-    preprocessing_function=preprocess_input,
-    horizontal_flip=False)
+    preprocessing_function=preprocess_input
+)
 
 validation_datagen = ImageDataGenerator(
-    preprocessing_function=preprocess_input,
-    horizontal_flip=False)
+    preprocessing_function=preprocess_input
+)
 
 BATCH_SIZE = 32
 EPOCHS = 100
@@ -37,13 +44,13 @@ train_generator = train_datagen.flow_from_directory(
     'keras_test/dataset/train/',
     target_size=(299, 299),
     batch_size=BATCH_SIZE,
-    class_mode='categorical')
+    class_mode='binary')
 
 validation_generator = validation_datagen.flow_from_directory(
     'keras_test/dataset/valid/',
     target_size=(299, 299),
     batch_size=BATCH_SIZE,
-    class_mode='categorical')
+    class_mode='binary')
 
 MODEL_FILE = 'filename.model'
 
@@ -54,6 +61,8 @@ history = model.fit_generator(
     train_generator,
     epochs=EPOCHS,
     validation_data=validation_generator,
+    steps_per_epoch=320,
+    validation_steps=200,
     verbose=1,
     callbacks=[earlyStopping, mcp_save]
 )
